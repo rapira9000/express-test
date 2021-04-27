a04fef0cb70d57ef68a5f26c220197ebd31f301e
Roman <rroommaann806@gmail.com>
2021-04-27 23:34:57 +0300

    dev

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    MONGO_DB_URI: `mongodb+srv://rapira9k:8Srpsaiqp0tj1RwM@cluster0.qn7sv.mongodb.net/reactTest?retryWrites=true&w=majority`,
-    TOKEN_USER_SECRET_KEY: 'sdasd#$@rt5yfdgdse453E#$5r',
-    SESSION_SECRET_KEY: 's748R43#$ifkr_0453fUrje&4ekd',
+    MONGO_DB_URI: ``,
+    TOKEN_USER_SECRET_KEY: '',
+    SESSION_SECRET_KEY: '',
     SESSION_MAX_AGE: 3600000,
     LISTEN_PORT: 3001,
     SES_NAME: 'sid',
